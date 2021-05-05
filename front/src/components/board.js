aa954ebc4995e80cca991d0a69d8bf58461b0039
SornrasakC <43643389+SornrasakC@users.noreply.github.com>
2021-05-06 01:20:15 +0700

    stiff4

--- front/src/components/board.js
+++ front/src/components/board.js
@@ -23,13 +23,15 @@ export default class Board extends React.Component {
         const squareShade = (isEven(i) && isEven(j)) || (!isEven(i) && !isEven(j)) ? "light-square" : "dark-square";
         squareRows.push(this.renderSquare((i * 8) + j, squareShade));
       }
-      squareRows.push(<span style={{}}>{8 - i}</span>)
+      squareRows.push(<div className="square">{8 - i}</div>)
       board.push(<div className="board-row" key={i}>{squareRows}</div>)
     }
+    board.push(<div className="board-row" key="alpha">{["A", "B", "C", "D", "E", "F", "G", "H"].map(letter => <div className="square">{letter}</div>)}</div>)
 
     return (
       <div>
         {board}
+        {/* <hr/> */}
       </div>
     );
   }
